12ee4be9eb53035ae6b7b6db9616575efdc7d68b
Deon <oluwadamilare_daniel@outlook.com>
2025-07-03 22:15:53 +0100

    repo init & gsap installation

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl font-bold underline text-red-500'>Hello Team1 Africa</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
