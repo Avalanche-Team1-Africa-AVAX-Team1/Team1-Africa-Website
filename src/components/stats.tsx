c250c1e606f6df4057bfc6aa247a031d73f512f7
DioChuks <diochuks65@gmail.com>
2025-09-17 17:47:25 +0100

    style: improve layout and typography in Stats component for mobile

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -70,26 +70,26 @@ const Stats = () => {
     }, [isVisible]);
 
     return(
-            <div ref={statsRef} className='relative flex h-screen flex-col justify-center items-center overflow-hidden'>
+            <div ref={statsRef} className='relative flex h-screen flex-col justify-around md:justify-center items-center overflow-hidden'>
                 <div className='flex justify-center items-center'>
-                    <p className='text-[50px] font-semibold text-center w-[50%]'>Team1 Africa <span className='text-[#6D6D6D]'>is a vibrant grassroots movement committed to equipping</span> African builders, creators, and educators with the tools, resources, and opportunities to thrive in the Avalanche blockchain ecosystem.</p>
+                    <p className='text-[1.4rem] md:text-[50px] font-semibold text-center md:w-[50%]'>Team1 Africa <span className='text-[#6D6D6D]'>is a vibrant grassroots movement committed to equipping</span> African builders, creators, and educators with the tools, resources, and opportunities to thrive in the Avalanche blockchain ecosystem.</p>
                 </div>
 
                 {/* positioned images */}
-                <img src={event1} alt="event1" width={300} height={150} className='absolute bottom-36 left-20' />
-                <img src={event2} alt="event2" width={300} height={150} className='absolute top-36 right-20' />
+                <img src={event1} alt="event1" width={300} height={150} className='hidden md:block absolute bottom-36 left-20' />
+                <img src={event2} alt="event2" width={300} height={150} className='hidden md:block absolute top-36 right-20' />
 
-                <div className='flex justify-center items-center gap-40 text-center'>
+                <div className='flex flex-col md:flex-row justify-center items-center gap-8 lg:gap-40 text-center'>
                     <div className='leading-tight'>
-                        <p className='text-[150px]'>{eventsCount}</p>
+                        <p className='text-[70px] xl:text-[150px]'>{eventsCount}</p>
                         <p className='text-xl font-semibold'>Events</p>
                     </div>
                     <div className='leading-tight'>
-                        <p className='text-[150px]'>{membersCount}+</p>
+                        <p className='text-[70px] xl:text-[150px]'>{membersCount}+</p>
                         <p className='text-xl font-semibold'>Community Members</p>
                     </div>
                     <div className='leading-tight'>
-                        <p className='text-[150px]'>{partnersCount}</p>
+                        <p className='text-[70px] xl:text-[150px]'>{partnersCount}</p>
                         <p className='text-xl font-semibold'>Game Partners</p>
                     </div>
                 </div>
