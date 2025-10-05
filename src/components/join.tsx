28e771098e1b08698e75d539f423bbe03961b937
DanielEmmanuel1 <oluwadamilare_daniel@outlook.com>
2025-10-05 19:29:30 +0100

    page settings

--- src/components/join.tsx
+++ src/components/join.tsx
@@ -6,45 +6,43 @@ import points from '../assets/Points.png'
 export default function join() {
     return (
         <div className="relative py-8 sm:py-12 md:py-16 lg:py-24 xl:py-32 2xl:py-40 hover:cursor-pointer group px-4 sm:px-6 md:px-8 lg:px-12">
-            {/* Main heading - centered container */}
-            <div className="flex items-center justify-center w-full">
-                <p 
-                    className="text-[32px] sm:text-[48px] md:text-[64px] lg:text-[96px] xl:text-[128px] 2xl:text-[160px] text-center text-muted leading-[1.2] max-w-[1400px]" 
+            {/* Main heading + decorative images - centered container */}
+            <div className="relative w-fit mx-auto">
+                <p
+                    className="text-[32px] sm:text-[48px] md:text-[64px] lg:text-[96px] xl:text-[128px] 2xl:text-[160px] text-center text-muted leading-[1.2] max-w-[1400px]"
                     style={{ fontFamily: "'Press Start 2P'" }}
                 >
                     JOIN OUR COMMUNITY
                 </p>
-            </div>
 
-            {/* Center Union Image */}
-            <div className="absolute inset-0 flex items-center justify-center z-10 pointer-events-none">
+                {/* Center Union Image (anchored to heading) */}
                 <img
                     src={union}
                     alt="union"
-                    className="transform rotate-[-2deg] w-[120px] sm:w-[180px] md:w-[240px] lg:w-[350px] xl:w-[450px] 2xl:w-[550px] transition-all duration-700 ease-in-out opacity-0 scale-50 group-hover:opacity-100 group-hover:scale-100"
+                    className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform rotate-[-2deg] w-[120px] sm:w-[160px] md:w-[220px] lg:w-[300px] xl:w-[380px] 2xl:w-[460px] transition-all duration-700 ease-in-out opacity-0 scale-50 group-hover:opacity-100 group-hover:scale-100 z-10"
+                />
+
+                {/* Points - near top-left of text */}
+                <img
+                    src={points}
+                    alt="points"
+                    className="pointer-events-none absolute -top-3 -left-3 sm:-top-4 sm:-left-4 md:-top-6 md:-left-6 lg:-top-8 lg:-left-8 w-8 sm:w-10 md:w-12 lg:w-14 xl:w-16 2xl:w-20 transition-all duration-700 ease-in-out opacity-0 -translate-y-2 -translate-x-2 group-hover:opacity-100 group-hover:translate-y-0 group-hover:translate-x-0"
+                />
+
+                {/* Arena - near top-right of text */}
+                <img
+                    src={arena}
+                    alt="arena"
+                    className="pointer-events-none absolute -top-3 -right-3 sm:-top-4 sm:-right-4 md:-top-6 md:-right-6 lg:-top-8 lg:-right-8 w-8 sm:w-10 md:w-12 lg:w-14 xl:w-16 2xl:w-20 transition-all duration-700 ease-in-out opacity-0 -translate-y-2 translate-x-2 group-hover:opacity-100 group-hover:translate-y-0 group-hover:translate-x-0"
+                />
+
+                {/* Dexalot - near bottom-left of text */}
+                <img
+                    src={dexalot}
+                    alt="dexalot"
+                    className="pointer-events-none absolute -bottom-3 -left-3 sm:-bottom-4 sm:-left-4 md:-bottom-6 md:-left-6 lg:-bottom-8 lg:-left-8 w-8 sm:w-10 md:w-12 lg:w-14 xl:w-16 2xl:w-20 transition-all duration-700 ease-in-out opacity-0 translate-y-2 -translate-x-2 group-hover:opacity-100 group-hover:translate-y-0 group-hover:translate-x-0"
                 />
             </div>
-
-            {/* Points - Top Left */}
-            <img
-                src={points}
-                alt="points"
-                className="absolute top-2 left-2 sm:top-4 sm:left-4 md:top-6 md:left-6 lg:top-8 lg:left-8 w-10 sm:w-14 md:w-16 lg:w-20 xl:w-24 transition-all duration-700 ease-in-out -translate-x-full -translate-y-full opacity-0 group-hover:translate-x-0 group-hover:translate-y-0 group-hover:opacity-100"
-            />
-
-            {/* Arena - Top Right */}
-            <img
-                src={arena}
-                alt="arena"
-                className="absolute top-2 right-2 sm:top-4 sm:right-4 md:top-6 md:right-6 lg:top-8 lg:right-8 w-10 sm:w-14 md:w-16 lg:w-20 xl:w-24 transition-all duration-700 ease-in-out translate-x-full -translate-y-full opacity-0 group-hover:translate-x-0 group-hover:translate-y-0 group-hover:opacity-100"
-            />
-
-            {/* Dexalot - Bottom Left */}
-            <img
-                src={dexalot}
-                alt="dexalot"
-                className="absolute bottom-2 left-2 sm:bottom-4 sm:left-4 md:bottom-6 md:left-6 lg:bottom-8 lg:left-8 w-10 sm:w-14 md:w-16 lg:w-20 xl:w-24 transition-all duration-700 ease-in-out -translate-x-full translate-y-full opacity-0 group-hover:translate-x-0 group-hover:translate-y-0 group-hover:opacity-100"
-            />
         </div>
     )
 }
