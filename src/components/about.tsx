390be6529a6034a017bf3622227ed5a80152ca71
DanielEmmanuel1 <oluwadamilare_daniel@outlook.com>
2026-01-02 08:30:13 +0100

    build error fix

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom'
 import communityImg from '../assets/community.png'
-import arrow from '../assets/white-arrow.svg'
 import arrowup from '../assets/arrow-up.svg'
 import AnimatedText from './AnimatedText'
 import MagneticButton from './MagneticButton'
